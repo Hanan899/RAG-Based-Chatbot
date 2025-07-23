e442a799dd1d89830bf4e769e7903d788d0791f9
Abdul Hanan <a.hananwork4@gmail.com>
2025-07-23 10:01:33 +0500

    Update text_splitter.py

--- utils/text_splitter.py
+++ utils/text_splitter.py
@@ -1,14 +1,17 @@
+from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-def split_documents(documents):
+def split_documents(extracted_lines):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
-        chunk_overlap=500,
+        chunk_overlap=100,
         length_function=len,
         add_start_index=True
     )
-    all_chunks = []
-    for doc in documents:
-        chunks = text_splitter.create_documents([doc["text"]], metadatas=[{"source": doc["name"]}])
-        all_chunks.extend(chunks)
-    return all_chunks
+
+    chunks = []
+    for line in extracted_lines:
+        docs = text_splitter.create_documents([line["text"]], metadatas=[line["metadata"]])
+        chunks.extend(docs)
+
+    return chunks
